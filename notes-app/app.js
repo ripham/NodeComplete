ecdf0f794be2897ab5a132852841bca69201a4ef
ripham <ripham@gmail.com>
2020-04-18 15:18:28 +0200

    Code challenge module export function

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
-const fs = require('fs')
+const getNotes = require('./notes.js')
 
-fs.writeFileSync('notes.txt', 'My name is Fran')
+console.log(getNotes())
 
-fs.appendFileSync('notes.txt', '\nFollowing a Node course')
+// const add = require('./utils.js')
+
+// const sum = add(4, -2)
+
+// console.log(sum)
