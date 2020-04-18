b937deb5c7be20053179aa58909b31bd4e85ee47
ripham <ripham@gmail.com>
2020-04-18 16:43:37 +0200

    Code challenge added yargs, reading arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,40 @@
-const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-console.log(getNotes())
+yargs.version = '1.1.0'
 
-console.log(validator.isURL('https://google.com'))
-console.log(chalk.green('Success!!!!'))
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function (){
+        console.log('Adding a new note!!!')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function (){
+        console.log('Removing a note!!!')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function (){
+        console.log('Listing all notes!!!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function (){
+        console.log('Reading a note!!!')
+    }
+})
+
+console.log(process.argv)
+console.log(yargs.argv)
